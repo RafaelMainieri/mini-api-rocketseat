0620ca27da3769e7edc96bc64ecaa77247bea86f
Rafael Mainieri <rafasolmainieri@gmail.com>
2024-11-23 16:53:39 -0300

    UserModel + UserController

---
+++ src/main/java/br/com/rafaelmainieri/todolist/user/UserModel.java
@@ -0,0 +1,21 @@
+package br.com.rafaelmainieri.todolist.user;
+
+
+public class UserModel {
+
+    private String username;
+    private String name;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
