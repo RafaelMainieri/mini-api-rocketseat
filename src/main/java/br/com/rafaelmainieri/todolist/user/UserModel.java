d4f604e9d7ad4d6657d78f69d70cfa641fe552cc
Rafael Mainieri <rafasolmainieri@gmail.com>
2024-11-23 17:22:03 -0300

    Tabela Usuário

--- src/main/java/br/com/rafaelmainieri/todolist/user/UserModel.java
+++ src/main/java/br/com/rafaelmainieri/todolist/user/UserModel.java
@@ -1,11 +1,30 @@
 package br.com.rafaelmainieri.todolist.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
+@Entity
+@Table(name = "tb_user")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    // id mais seguro
 
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    // atributo para saber quando o User foi criado
 }
