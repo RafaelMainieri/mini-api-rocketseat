e7dd7857b5894541bf8e81a22e23d1826aa5ea5b
Rafael Mainieri <rafasolmainieri@gmail.com>
2024-11-23 18:20:39 -0300

    TaskModel + TaskController

---
+++ src/main/java/br/com/rafaelmainieri/todolist/task/TaskController.java
@@ -0,0 +1,24 @@
+package br.com.rafaelmainieri.todolist.task;
+
+import org.springframework.scheduling.config.Task;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private TaskRepository taskRepository;
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @PostMapping
+    public TaskModel create(@RequestBody TaskModel taskModel) {
+        var task = this.taskRepository.save(taskModel);
+        return task;
+    }
+}
